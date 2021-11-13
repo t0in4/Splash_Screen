24bcbdfc6f533297c981f8bc6924630283fd5347
t0in4 <t0in4@yahoo.com>
2021-11-13 15:40:07 +0300

    splash screen with layout-list

---
+++ app/src/main/java/com/eyehail/testing4/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.eyehail.testing4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.google.android.gms.instantapps.Launcher
+
+class MainActivity : AppCompatActivity() {
+    private val mHandler = Handler(Looper.getMainLooper())
+
+    private val mLauncher: Launcher = Launcher()
+    override fun onStart() {
+        super.onStart()
+        mHandler.postDelayed(mLauncher, SPLASH_DELAY.toLong())
+    }
+
+    override fun onStop() {
+        mHandler.removeCallbacks(mLauncher)
+        super.onStop()
+    }
+
+    private fun launch() {
+        if (!isFinishing) {
+            startActivity(Intent(this@MainActivity, WelcomeActivity::class.java))
+            finish()
+        }
+    }
+
+
+    private inner class Launcher : Runnable {
+        override fun run() {
+            launch()
+        }
+    }
+
+    companion object {
+        private const val SPLASH_DELAY = 2000
+        private const val TAG = "MainActivity"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
---
+++ app/src/main/java/com/eyehail/testing4/WelcomeActivity.kt
@@ -0,0 +1,18 @@
+package com.eyehail.testing4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.google.android.gms.instantapps.Launcher
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+    }
+
+}
